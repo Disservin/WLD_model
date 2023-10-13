201322e7faf17286c4594bc9cb0e8aee047e8a5a
Disservin <disservin.social@gmail.com>
2023-10-13 08:08:42 +0200

    change to gzstream library

--- scoreWDLstat.cpp
+++ scoreWDLstat.cpp
@@ -15,6 +15,7 @@
 #include <vector>
 
 #include "external/chess.hpp"
+#include "external/gzip/gzstream.h"
 #include "external/threadpool.hpp"
 
 namespace fs = std::filesystem;
@@ -227,7 +228,7 @@ void ana_files(map_t &map, const std::vector<std::string> &files, const std::str
         };
 
         if (file.size() >= 3 && file.substr(file.size() - 3) == ".gz") {
-            GzippedFileIStream input(file.c_str());
+            igzstream input(file.c_str());
             pgn_iterator(input);
         } else {
             std::ifstream pgn_stream(file);
